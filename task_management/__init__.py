9931c7042e5d4d4a22e8b5629938560ac73c4c9e
iShowError <adityakiratsata@gmail.com>
2025-09-25 16:10:15 +0530

    - Added Django project structure with settings - Configured database and REST framework

---
+++ task_management/__init__.py
@@ -0,0 +1,7 @@
+"""
+Django Project Package Initialization
+
+In order to enable Django to import project-level modules and customizations,
+this file designates the task_management directory as a Python package.
+Without this file, imports like 'from task_management import settings' would fail.
+"""
