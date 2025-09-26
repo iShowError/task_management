2e60495401a040cf34f641f315ed307c2f167191
iShowError <adityakiratsata@gmail.com>
2025-09-26 12:34:19 +0530

    - Added API endpoints for user registration and authentication

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
+from rest_framework.authtoken import views as authtoken_views
 
 router = DefaultRouter()
 router.register(r'tasks', views.TaskViewSet)
 router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
+    path('api/register/', views.UserRegistrationView.as_view(), name='register'),
+    path('api/api-token-auth/', authtoken_views.obtain_auth_token, name='api-token-auth'),
     path('api/', include(router.urls)),
 ]
